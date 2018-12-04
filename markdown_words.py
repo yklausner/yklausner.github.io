cafa100d6700dbefa42b633de2aa213bf087fed6
Yonatan Klausner <YoniKlausner@Yonatans-MacBook-Pro.local>
2018-12-03 23:59:33 -0500

    adding files

---
+++ markdown_words.py
@@ -0,0 +1,20 @@
+from json import load
+from sys import argv
+
+def loc(nb):
+    cells = load(open(nb))['cells']
+    #return sum(len(c['source']) for c in cells if c['cell_type'] == 'markdown')
+    sum = 0
+    for c in cells:
+    	if c['cell_type'] == 'markdown':
+    		for line in c['source']:
+    			sum += len(line.split(' '))
+
+    return sum
+
+
+def run(ipynb_files):
+    return sum(loc(nb) for nb in ipynb_files)
+
+if __name__ == '__main__':
+    print(run(argv[1:]))
